12d70caf179198f14926b833c2ba9974cb7a7ca4
alesandramempis <alesandramempis@gmail.com>
2021-09-22 10:20:01 -0300

    Criação do component jogo da velha e implementação da regra do jogo no service

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { JogoDaVelhaModule } from './jogo-da-velha';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        JogoDaVelhaModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { JogoDaVelhaModule } from './jogo-da-velha';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    JogoDaVelhaModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
